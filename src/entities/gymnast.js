9ebd5e3abf86d8b7061092a6712c1b633d3c73ca
Gregory Brinson <gcb131312@gmail.com>
2024-08-09 23:30:06 -0400

    started release physics:

--- src/entities/gymnast.js
+++ src/entities/gymnast.js
@@ -12,14 +12,14 @@ export function makeGymnast(
 
     const GRAVITY = 1;
     const FRICTION_MULTIPLIER = 0.98;
-    const KICK_FORCE = 18;
+    const KICK_FORCE = 17;
 
     //create the player object
     const player = k.make([
         k.rect(PLAYER_WIDTH, PLAYER_HEIGHT),
         k.pos(PLAYER_X, PLAYER_Y),
         k.color(255, 243, 12),
-        k.rotate(-90),
+        k.rotate(0),
         k.anchor("top"),
         k.body(),
         k.area(),
@@ -30,34 +30,53 @@ export function makeGymnast(
     // let previousAngle = 0;
     let kickAngularAcceleration = 0;
     // let timeSinceLastKick = 0;
+    let playerIsAirborne = false;
+    let linearVelocity = 0;
 
     //set all the keybindings
     k.onKeyPress("space", () => {
         // const currentTime = time();
-        kickAngularAcceleration = (-KICK_FORCE * Math.cos(Math.PI / 4)) / 1.5;
-        player.height = PLAYER_HEIGHT / 2;
+        if (!playerIsAirborne) {
+            kickAngularAcceleration =
+                (-KICK_FORCE * Math.cos(Math.PI / 4)) / 1.5;
+        }
+    });
+
+    k.onKeyPress("enter", () => {
+        // const currentTime = time();
+        playerIsAirborne = true;
+        console.log(player.angle);
     });
 
     //might pull this out into its own function for general spin physics stuff
     //this is called about 60 times per second
     player.onUpdate(() => {
-        angularAcceleration =
-            -(GRAVITY * Math.sin(player.angle * (Math.PI / 180))) / 1.5;
-        angularAcceleration += kickAngularAcceleration;
-        kickAngularAcceleration = 0;
+        //player is swinging
+        if (!playerIsAirborne) {
+            angularAcceleration =
+                -(GRAVITY * Math.sin(player.angle * (Math.PI / 180))) / 1.5;
+            angularAcceleration += kickAngularAcceleration;
+            kickAngularAcceleration = 0;
 
-        // if (player.angle > 0 && previousAngle < 0) angularAcceleration *= -1;
-        // else if (player.angle < 0 && previousAngle > 0) angularAcceleration *= -1;
-        // else if (player.angle > 180 && previousAngle < 180) angularAcceleration *= -1;
-        // else if (player.angle < 180 && previousAngle > 180) angularAcceleration *= -1;
+            // if (player.angle > 0 && previousAngle < 0) angularAcceleration *= -1;
+            // else if (player.angle < 0 && previousAngle > 0) angularAcceleration *= -1;
+            // else if (player.angle > 180 && previousAngle < 180) angularAcceleration *= -1;
+            // else if (player.angle < 180 && previousAngle > 180) angularAcceleration *= -1;
 
-        //update all the things
-        angularVelocity += angularAcceleration;
-        angularVelocity *= FRICTION_MULTIPLIER;
+            //update all the things
+            angularVelocity += angularAcceleration;
+            angularVelocity *= FRICTION_MULTIPLIER;
 
-        if (angularVelocity < 0.001 && angularVelocity > -0.001)
-            angularVelocity = 0;
-        player.angle += angularVelocity;
+            if (angularVelocity < 0.001 && angularVelocity > -0.001)
+                angularVelocity = 0;
+            player.angle += angularVelocity;
+            player.angle %= 360;
+        }
+        //player is airborne
+        else {
+            linearVelocity = Math.sqrt(2*angularAcceleration*Math.abs(player.angle));
+            // player.move(linearVelocityX, linearVelocityY);
+        }
     });
 
     return player;
